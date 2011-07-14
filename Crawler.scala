81789c4f83b7c1adbdc6d4712981da4af5568513
Cem Eliguzel <cem@eliguzel.net>
2011-07-15 00:18:46 +0300

    Ignore books whose price is defined in currencies other than TL

--- Crawler.scala
+++ Crawler.scala
@@ -7,7 +7,7 @@ import util.Random
 
 object Crawler {
 	
-	val SITE_URL 			= "http://www.pandora.com.tr/"
+	val SITE_URL 			= "http://www.pandora.com.tr"
 	val BOOK_NAME_PATH 		= "div.kitaptitle2 > h1"
 	val BOOK_PRICE_PATH 	= "span.fiyat"
 	val BOOK_ISBN_PATH 		= "span#ContentPlaceHolderMainOrta_LabelIsbn"
@@ -22,11 +22,19 @@ object Crawler {
 		val bookPrice 	= doc.select(BOOK_PRICE_PATH).first()
 		val isbn  		= doc.select(BOOK_ISBN_PATH).first()
 
+		val txtBookPrice = try {
+			val Price = """(\S+) TL""".r
+			val Price(price) = bookPrice.text()
+			price
+		} catch {
+			case e : MatchError => println("Price information is not in TL"); return
+		}
+
 		try {
 			println("------- " + title + " -------")
 			println("Kitap = " + bookName.text())
 			println("ISBN  = " + isbn.text())
-			println("Fiyat = " + bookPrice.text())
+			println("Fiyat = " + txtBookPrice + " TL")
 		} catch {
 			case e : NullPointerException => println("Düzgün biçimli kitap bilgisi bulunamadı.")
 		}
