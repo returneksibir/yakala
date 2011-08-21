1d7d26cd0bd5c017dc9621f9f480e34cb7a0fb43
Sinan Nalkaya <sardok@gmail.com>
2011-08-21 19:39:10 +0300

    Crawler refDb use try instead of Set, to see if it helps memory and slowness problem.

--- yakala/crawler/Crawler.scala
+++ yakala/crawler/Crawler.scala
@@ -4,29 +4,40 @@ import yakala.logging.Logger
 import yakala.pipelines.ItemPipeline
 import yakala.spiders.Spider
 import yakala.registery._
-import collection.mutable.Set
+import yakala.refdb._
 import collection.immutable.Map
 import scala.actors.Actor
 import scala.actors.Actor._
 
 
 class Crawler(logger : Logger) extends Actor {
-  private var setOfLinksAlreadyVisited : Set[String] = Set()
+  private val linkDb = new refDbH2("CrawledLinks")
   private val thisActor = this.start
+  private var counter = 0
 
   def act() {
     loop {
       react {
 	case url: String => {
-	  if (!setOfLinksAlreadyVisited.contains(url)) {
-	    setOfLinksAlreadyVisited  += url
+	  println("Crawler mailboxSize : " + mailboxSize + ", url : " + url)
+	  val urlId = url.hashCode
+	  if (!linkDb.hasRef(urlId)) {
+	    linkDb.addRef(new Ref(urlId, url))
 	    val consumers = Registery.filter(url)
-	    logger.debug("[Crawler] Pushing url : " + url + " (pushed url count : " + setOfLinksAlreadyVisited.size + ")")
+	    counter += 1
+	    logger.debug("[Crawler] Pushing url : " + url + " (pushed url count : " + counter + ")")
 	    consumers.foreach( _ ! (thisActor, url) )
 	  }
+	  // if (!setOfLinksAlreadyVisited.contains(url)) {
+	  //   setOfLinksAlreadyVisited  += url
+	  //   val consumers = Registery.filter(url)
+	  //   logger.debug("[Crawler] Pushing url : " + url + " (pushed url count : " + setOfLinksAlreadyVisited.size + ")")
+	  //   consumers.foreach( _ ! (thisActor, url) )
+	  // }
 	}
 
 	case vl: Any => {
+	  println("Crawler mailboxSize : " + mailboxSize + ", data : " + vl)
 	  val consumers = Registery.filter(vl)
 	  logger.debug("[Crawler] Consumer list: '" + consumers + "', relevant data : " + vl)
 	  consumers.foreach( _ ! (thisActor, vl) )
