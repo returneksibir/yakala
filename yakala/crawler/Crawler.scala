06b769ca6533351a6bd12bde87ebb4503821b939
Sinan Nalkaya <sardok@gmail.com>
2011-09-07 01:19:38 +0300

    Added alternative simple ref db to crawler.

--- yakala/crawler/Crawler.scala
+++ yakala/crawler/Crawler.scala
@@ -5,13 +5,42 @@ import yakala.pipelines.ItemPipeline
 import yakala.spiders.Spider
 import yakala.registery._
 import collection.mutable.Set
-import collection.immutable.Map
 import scala.actors.Actor
 import scala.actors.Actor._
 
+sealed class CrawlerLinkDb {
+  var db: List[Set[String]] = List(Set.empty)
+  val limit = 1000000
+  
+  def contains(v: String): Boolean = {
+    val currDb: Set[String] = db.head
+    currDb.contains(v)
+  }
+
+  def add(v: String) {
+    db.foreach{ currDb =>
+      if (currDb.contains(v)) {
+	true
+      }
+    }
+    if (db.head.size > limit) {
+      val newHead: Set[String] = Set.empty
+      db = newHead :: db
+    }
+    db.head.add(v)
+  }
+
+  def += = this.add _
+
+  def size = {
+    val sizeList = for (list <- db) yield list.size
+    sizeList.sum
+  }
+}
 
 class Crawler(logger : Logger) extends Actor {
-  private var setOfLinksAlreadyVisited : Set[String] = Set()
+//  private val setOfLinksAlreadyVisited : Set[String] = Set()
+  private val setOfLinksAlreadyVisited = new CrawlerLinkDb
   private val thisActor = this.start
 
   def act() {
