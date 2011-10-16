d7a84136a1c8d49659c0d128a3aa298a9bb347bf
Cem Eliguzel <cem@eliguzel.net>
2011-10-16 21:12:28 +0300

    Fix for issue #23 : Need for user agent info

--- yakala/spiders/Spider.scala
+++ yakala/spiders/Spider.scala
@@ -84,7 +84,7 @@ trait Spider extends Actor {
 
       logger.info("Sayfa :" + url)
   
-      val doc = Jsoup.connect(url).get()
+      val doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").get()
   
       if (isProductPage(url)) {
         try {
