d1533c677b48965f521fe030f8f9c03d1f20860f
Cem Eliguzel <cemeliguzel@gmail.com>
2013-02-22 21:09:43 +0200

    Moves generic collecting statistics information to ItemPipeline class, which requires passing Logger instance to the constructore of ItemPipeline

--- yakala/pipelines/ItemPipeline.scala
+++ yakala/pipelines/ItemPipeline.scala
@@ -5,16 +5,36 @@ import scala.actors.Actor
 import scala.actors.Actor._
 
 
-abstract class ItemPipeline extends Actor {
+abstract class ItemPipeline(logger : Logger) extends Actor {
   def processItem(item : Map[String, String])
   
+
+  var numberOfProcessedItems : Long = 0;
+  var numberOfExceptions : Long = 0;
+
   def act() {
     loop {
       react {
-        case item : Map[String, String]       => processItem(item)
+        case item : Map[String, String]       =>
+          numberOfProcessedItems += 1
+          try {
+            processItem(item)
+          } catch {
+            case e  =>
+            numberOfExceptions += 1
+            logger.debug("Exception :" + e.getMessage())
+          }
+
         case _                                => require(false)
       }
     }
   }
+
+  def printStats(print : String => Unit) {
+    print("\n# messages in mailbox = " + this.mailboxSize)
+    print("\n# processed items = " + numberOfProcessedItems)
+    print("\n# exceptions = " + numberOfExceptions)
+  }
+
 }
 
